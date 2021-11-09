1406009c1709aba2651cc4c546e9e33b27974538
Eva Mondragon <emondra1@progressive.com>
2021-11-09 15:29:39 -0500

    Add project files.

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal _balance = 5000M;
+
+        public decimal GetBalance()
+        {
+            return _balance;
+            //return balance; //sliming --> you know that's not where it's gonna end up
+            //             //but it works for now, then refactor and make more real
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+            _balance -= amountToWithdraw;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            _balance += amountToDeposit;
+        }
+    }
+}
